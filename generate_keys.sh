7983b22ee7c01522959f47813e310ae3ea6058c1
Aaron Ogle <aaron@geekgonecrazy.com>
2022-05-27 15:47:23 -0500

    Initial

---
+++ generate_keys.sh
@@ -0,0 +1,7 @@
+docker run --rm --entrypoint="" \
+  -v $(pwd)/config:/mnt \
+  matrixdotorg/dendrite-monolith:latest \
+  /usr/bin/generate-keys \
+  -private-key /mnt/matrix_key.pem \
+  -tls-cert /mnt/server.crt \
+  -tls-key /mnt/server.key
